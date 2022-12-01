5e7c094fbcccf3b3064ad869da76f3bc4d8d29fd
PJ <pierre-joseph.beaugendre@epitech.eu>
2022-12-01 18:45:33 +0000

    [~] fix position

--- src/pages/AdminAddItems.jsx
+++ src/pages/AdminAddItems.jsx
@@ -6,10 +6,17 @@ const AdminAddItems = () => {
     document.title = 'Admin add items';
   }, []);
 
+  const toHome = () => {
+    document.location.href = "/"
+  }
+
   return (
-    <div>
-      AddItems
-    </div>
+    <>
+      {/*<button onClick={toHome} className="fixed top-5 right-0 mr-64 text-white bg-red-700 hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Disconnect</button>*/}
+      <div>
+        AddItems
+      </div>
+    </>
   )
 }
 
--- src/pages/AdminHome.jsx
+++ src/pages/AdminHome.jsx
@@ -6,8 +6,13 @@ const AdminHome = () => {
     document.title = 'Admin Home';
   }, []);
 
+  const toHome = () => {
+    document.location.href = "/"
+  }
+
   return (
     <>
+      {/*<button onClick={toHome} className="fixed top-5 right-0 mr-64 text-white bg-red-700 hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Disconnect</button>*/}
       <div className='flex flex-row justify-center items-center content-center pt-72 text-center space-x-24'>
         <a href="/AdminAddItems">
           <div className="flex justify-center items-center content-center mb-6 w-48 h-48 border-2 border-secondary rounded-lg shadow-2xl hover:shadow-none hover:bg-gray-200">
