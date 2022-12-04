770723c9c2f5395e488d551e2b2e73f40a201cd3
Alexandre Bidault <alexandre.bidault@epitech.eu>
2022-12-04 15:57:13 +0000

    remove useless code

--- src/pages/items/CreateItems.jsx
+++ src/pages/items/CreateItems.jsx
@@ -9,14 +9,13 @@ const CreateItems = () => {
     const [type, setType] = useState("");
     const [quantity, setQuantity] = useState("");
     const [expi, setExpi] = useState("");
-    const [alert, setAlert] = useState(false);
 
     const ref = collection(firestore, "items");
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log('name = %s, type = %s, quantity = %d, expiration date = %s', name, type, quantity, expi);
-        
+
         let data = {
             name: name,
             type: type,
@@ -36,7 +35,7 @@ const CreateItems = () => {
                 title: 'Item added successfully',
                 showConfirmButton: false,
                 timer: 1500
-              })
+            })
         } catch {
             console.log(e);
         }
@@ -44,7 +43,7 @@ const CreateItems = () => {
     return (
         <div className='flex flex-col'>
             <div className='justify-left'>
-            <button onClick={() => document.location.href = "/AdminHome"} className="
+                <button onClick={() => document.location.href = "/AdminHome"} className="
       px-6
       py-2.5
       m-8
@@ -63,18 +62,11 @@ const CreateItems = () => {
       duration-150
       ease-in-out">Return</button>
             </div>
-        <div className='flex flex-col justify-center items-center content-center pt-32 text-center space-x-24'>
-            {/* { alert ?
-            <div className="alert bg-green-100 rounded-lg ml-20 py-5 px-6 mb-3 text-base text-green-700 alert-dismissible fade show" role="alert">
-            <strong className="mr-1">Item created ! </strong> you can close this pop-up.
-            <button type="button" className="btn-close box-content w-4 h-4 p-1 ml-auto text-green-900 border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:green-900 hover:opacity-75 hover:no-underline" data-bs-dismiss="alert" aria-label="Close" onClick={() => {setAlert(false)}}></button>
-          </div> :
-            false
-            } */}
-            <form onSubmit={handleSubmit}>
-                <div className="form-group mb-6">
-                    <label className="form-label inline-block mb-2 text-gray-700">Item Name</label>
-                    <input type="text" className="
+            <div className='flex flex-col justify-center items-center content-center pt-32 text-center space-x-24'>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group mb-6">
+                        <label className="form-label inline-block mb-2 text-gray-700">Item Name</label>
+                        <input type="text" className="
         form-control
         block
         w-full
@@ -91,16 +83,16 @@ const CreateItems = () => {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-                        id="NameItem"
-                        placeholder="Name"
-                        value={name} onChange={(e) => setName(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="form-group mb-6">
+                            id="NameItem"
+                            placeholder="Name"
+                            value={name} onChange={(e) => setName(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="form-group mb-6">
 
-                    <label className="form-label inline-block mb-2 text-gray-700">Category</label>
-                    <select className="form-select appearance-none
+                        <label className="form-label inline-block mb-2 text-gray-700">Category</label>
+                        <select className="form-select appearance-none
                 block
                 w-full
                 px-3
@@ -115,16 +107,16 @@ const CreateItems = () => {
                 ease-in-out
                 m-0
                 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example"
-                        value={type} onChange={(e) => setType(e.target.value)} required>
-                        <option selected>Select menu</option>
-                        <option value="Luxury">Luxury</option>
-                        <option value="Essential">Essential</option>
-                        <option value="Gift">Gift</option>
-                    </select>
-                </div>
-                <div className="form-group mb-6">
-                    <label className="form-label inline-block mb-2 text-gray-700">Quantity Item</label>
-                    <input type="number" className="
+                            value={type} onChange={(e) => setType(e.target.value)} required>
+                            <option selected>Select menu</option>
+                            <option value="Luxury">Luxury</option>
+                            <option value="Essential">Essential</option>
+                            <option value="Gift">Gift</option>
+                        </select>
+                    </div>
+                    <div className="form-group mb-6">
+                        <label className="form-label inline-block mb-2 text-gray-700">Quantity Item</label>
+                        <input type="number" className="
         form-control
         block
         w-full
@@ -141,22 +133,22 @@ const CreateItems = () => {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-                        id="QuantityItem"
-                        placeholder="5"
-                        value={quantity} onChange={(e) => setQuantity(e.target.value)}
-                        required
-                    />
-                </div>
-                <label className="form-label inline-block mb-2 text-gray-700">Expiration Date</label>
-                <div className="datepicker relative form-floating mb-3 xl:w-96">
-                    <input type="date"
-                        className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                        placeholder="Select a date: dd/mm/yyyy" value={expi} onChange={(e) => setExpi(e.target.value)}
-                        data-mdb-toggle="datepicker"
-                        required />
-                    <label for="floatingInput" className="text-gray-700">Select a date</label>
-                </div>
-                <button type="submit" className="
+                            id="QuantityItem"
+                            placeholder="5"
+                            value={quantity} onChange={(e) => setQuantity(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <label className="form-label inline-block mb-2 text-gray-700">Expiration Date</label>
+                    <div className="datepicker relative form-floating mb-3 xl:w-96">
+                        <input type="date"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            placeholder="Select a date: dd/mm/yyyy" value={expi} onChange={(e) => setExpi(e.target.value)}
+                            data-mdb-toggle="datepicker"
+                            required />
+                        <label for="floatingInput" className="text-gray-700">Select a date</label>
+                    </div>
+                    <button type="submit" className="
       w-full
       px-6
       py-2.5
@@ -174,8 +166,8 @@ const CreateItems = () => {
       transition
       duration-150
       ease-in-out">Add Item</button>
-            </form>
-        </div>
+                </form>
+            </div>
         </div>
     )
 }
--- src/pages/items/ViewItems.jsx
+++ src/pages/items/ViewItems.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { collection, deleteDoc, getDocs, doc} from "firebase/firestore";
+import { collection, deleteDoc, getDocs, doc } from "firebase/firestore";
 import { firestore } from "../../firebase";
 
 const ViewItems = () => {
@@ -33,7 +33,7 @@ const ViewItems = () => {
     return (
         <div>
             <div>
-            <button onClick={() => document.location.href = "/AdminHome"} className="
+                <button onClick={() => document.location.href = "/AdminHome"} className="
       px-6
       py-2.5
       m-8
@@ -53,25 +53,25 @@ const ViewItems = () => {
       ease-in-out">Return</button>
             </div>
             <div className='flex flex-wrap justify-center'>
-            
-            {items?.map((item, i) => (
-                <div key={i} className="p-6 rounded-lg shadow-lg m-6 bg-white w-2/6 space-x-6">
-                    <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">{item.name}</h5>
-                    <p className="text-gray-700 text-base mb-4">
-                        Category : {item.type}
-                    </p>
-                    <p className="text-gray-700 text-base mb-4">
-                        Quantity : {item.quantity}
-                    </p>
-                    <p className="text-gray-700 text-base mb-4">
-                        Expiration Date : {item.expi}
-                    </p>
-                    <button type="button" id={i} onClick={deleteItem} className=" inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
-                </div>
-            ))}
+
+                {items?.map((item, i) => (
+                    <div key={i} className="p-6 rounded-lg shadow-lg m-6 bg-white w-2/6 space-x-6">
+                        <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">{item.name}</h5>
+                        <p className="text-gray-700 text-base mb-4">
+                            Category : {item.type}
+                        </p>
+                        <p className="text-gray-700 text-base mb-4">
+                            Quantity : {item.quantity}
+                        </p>
+                        <p className="text-gray-700 text-base mb-4">
+                            Expiration Date : {item.expi}
+                        </p>
+                        <button type="button" id={i} onClick={deleteItem} className=" inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                    </div>
+                ))}
+            </div>
         </div>
-        </div>
-       
+
     )
 }
 
