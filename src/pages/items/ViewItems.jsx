4a7d69548ca4772e365f5c74ac55946604b45726
Alexandre Bidault <alexandre.bidault@epitech.eu>
2022-12-04 16:04:20 +0000

    change the name of delete button in view admin

--- src/pages/items/ViewItems.jsx
+++ src/pages/items/ViewItems.jsx
@@ -66,7 +66,7 @@ const ViewItems = () => {
                         <p className="text-gray-700 text-base mb-4">
                             Expiration Date : {item.expi}
                         </p>
-                        <button type="button" id={i} onClick={deleteItem} className=" inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                        <button type="button" id={i} onClick={deleteItem} className=" inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Delete</button>
                     </div>
                 ))}
             </div>
