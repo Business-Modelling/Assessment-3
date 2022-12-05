b9a6145b42ccb6499629fb1d60f7bf7ba7682993
PJ <pierre-joseph.beaugendre@epitech.eu>
2022-12-05 23:02:56 +0000

    [+] select of the categorie

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", './node_modules/tw-elements/dist/js/**/*.js'
+    "./src/**/*.{js,jsx,ts,tsx}", './node_modules/tw-elements/dist/js/**/*.js',
+    "./node_modules/react-tailwindcss-select/dist/index.esm.js"
   ],
   theme: {
     extend: {},
