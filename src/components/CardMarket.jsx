1493ec1bd75e16c9cc7fddbb6eca833b853fca38
s1mpleTEK <57884979+s1mpleTEK@users.noreply.github.com>
2022-12-07 09:48:19 +0000

    add(:input.number): for the personalization of quantity

--- src/components/CardMarket.jsx
+++ src/components/CardMarket.jsx
@@ -30,8 +30,16 @@ const CardMarket = ({name, price, date, vat, quantity,addItemToBasket}) => {
             </div>
             <div>
             {vat}
-            </div>
           </div>
+        </div>
+        <div className='flex justify-between '>
+            <div>
+              Qantity: 
+            </div>
+            <div>
+              <input type="number" id="visitors" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[50%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={quantity} />
+            </div>
+        </div>
           <button className='w-fit px-3 py-2 self-center rounded-lg bg-secondary text-white font-medium leading-tight shadow-md hover:bg-primary hover:shadow-lg focus:bg-primary focus:shadow-lg focus:outline-none focus:ring-0 active:bg-midnight active:shadow-lg transition duration-150 ease-in-out'
             onClick={() => addItemToBasket(name, vat, price, quantity)}
           >
