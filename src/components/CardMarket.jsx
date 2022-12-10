16264ebe61a86c296d2b62863f269f448fcbc5b9
Quentin Nicolas <quentin.nicoas@epitech.eu>
2022-12-10 18:31:50 +0000

    [MOD] calculate button

--- src/components/CardMarket.jsx
+++ src/components/CardMarket.jsx
@@ -58,7 +58,7 @@ const CardMarket = ({name, price, date, vat, quantity, type, addItemToBasket, ca
             Add to basket
           </button>
           <button className='w-fit px-3 py-2 self-center rounded-lg bg-secondary text-white font-medium leading-tight shadow-md hover:bg-primary hover:shadow-lg focus:bg-primary focus:shadow-lg focus:outline-none focus:ring-0 active:bg-midnight active:shadow-lg transition duration-150 ease-in-out'
-                  onClick={calculate(type, quantity)}
+                  onClick={() => {calculate(type, value)}}
           >
             Calculate
           </button>
